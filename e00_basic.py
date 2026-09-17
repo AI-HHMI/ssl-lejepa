25c105390dcdc9146396dd3d8178ffc0a4f64f9c
Coleman <coleman.broaddus@gmail.com>
2026-09-17 15:18:24 -0400

    real lmd data

--- e00_basic.py
+++ e00_basic.py
@@ -3,10 +3,13 @@ import lmd_catalog as lmd
 from miao.config import MiaoConfig
 from miao import VolumeDataset
 from rich import print as pprint
-import os
+import os, sys
 
 lmd.set_data_root("/Volumes/miaai/lmd-v0.0.1/data")
-volumes = [x.to_miao() for x in lmd.all() if "flyliconn" in x.name]
+# volumes = [x.to_miao() for x in lmd.all() if "flyliconn" in x.name]
+volumes = [x.to_miao() for x in lmd.all() if x.name == "exm-drosophila-flyliconn-matt-260601-60X-B4-2-045/crop-001"]
+pprint(volumes)
+
 mcfg = MiaoConfig(
     volumes=volumes,
     patch_size=[104, 232, 232],
@@ -16,14 +19,13 @@ mcfg = MiaoConfig(
     output_axes="lzyx",
 )
 dl = VolumeDataset(mcfg)
-pprint(mcfg)
-pprint(dl[0])
-
-import sys
-sys.exit(0)
+# pprint(mcfg)
+# pprint(dl[0])
 
 savedir = "outdir/e00/main/basic/"
 os.makedirs(savedir, exist_ok=True)
+
+import torch
 
 cfg = LejepaConfig(
     n_layers = 12,
@@ -34,21 +36,20 @@ cfg = LejepaConfig(
 model = Lejepa(cfg)
 pprint(model)
 
-import torch
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"We're using torch device {device} .")
 net = model.to(device)
 opt = torch.optim.Adam(model.parameters(), lr = 1e-4)
+# init_weights(net)
 
 for ep in range(100):
-    x = torch.rand(mcfg.patch_size)
-    out = model(x)
+    # x = torch.rand(mcfg.patch_size)
+    x = dl[ep]
+    out = model(x['img'])
     out.loss.backwards()
-    # init_weights(net)
     opt.step()
     opt.zero_grad()
     print("\033[F",end='') ## move cursor UP one line 
     # print(f"finished epoch {ep+1}/{100}, loss={torch.rand():4f}, dt={dt:4f}, rate={N_pix/dt:5f} Mpix/s", end='\n',flush=True)
-    print(f"finished epoch {ep+1}/{100}, loss={torch.rand():4f},", end='\n',flush=True)
+    print(f"finished epoch {ep+1}/{100}, loss={out.loss.detach():4f},", end='\n',flush=True)
 
