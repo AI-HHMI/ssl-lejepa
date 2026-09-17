f67bb499b8fae3cede3abbd79ac0bb497284ab90
Coleman <coleman.broaddus@gmail.com>
2026-09-17 14:37:43 -0400

    fix

--- e00_basic.py
+++ e00_basic.py
@@ -3,6 +3,7 @@ import lmd_catalog as lmd
 from miao.config import MiaoConfig
 from miao import VolumeDataset
 from rich import print as pprint
+import os, shutil
 
 volumes = [x.to_miao() for x in lmd.all() if "flyliconn" in x.name]
 mcfg = MiaoConfig(
@@ -17,6 +18,7 @@ mcfg = MiaoConfig(
 pprint(mcfg)
 
 savedir = "outdir/e00/main/basic/"
+os.makedirs(savedir, exist_ok=True)
 
 cfg = LejepaConfig(
     n_layers = 12,
@@ -27,5 +29,21 @@ cfg = LejepaConfig(
 model = Lejepa(cfg)
 pprint(model)
 
+import torch
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"We're using torch device {device} .")
+opt = torch.optim.Adam(model.parameters(), lr = 1e-4)
+
+for ep in range(100):
+    x = torch.rand(mcfg.patch_size)
+    out = model(x)
+    out.loss.backwards()
+    net = model.to(device)
+    # init_weights(net)
+    opt.step()
+    opt.zero_grad()
+    print("\033[F",end='') ## move cursor UP one line 
+    # print(f"finished epoch {ep+1}/{100}, loss={torch.rand():4f}, dt={dt:4f}, rate={N_pix/dt:5f} Mpix/s", end='\n',flush=True)
+    print(f"finished epoch {ep+1}/{100}, loss={torch.rand():4f},", end='\n',flush=True)
 
